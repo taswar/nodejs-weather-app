36fb0fa5d9c16d8ed6387036721bff203d8c832e
TUser <taswar.bhatti@gemalto.com>
2018-03-23 13:58:27 -0400

    use env variable for APIKEY

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
-let apiKey = 'xyz'; 
+let apiKey = process.env.APIKEY;
 app.set('apiKey', apiKey);
 
 // catch 404 and forward to error handler
